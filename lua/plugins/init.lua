4141924f9260a844e6b719903c8daa042b46eca6
gubnik <danger8217.nik@gmail.com>
2025-10-22 10:14:54 +0300

    Change to new LSP config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,7 +7,6 @@ return {
     {
         "mfussenegger/nvim-jdtls",
     },
-    -- These are some examples, uncomment them if you want to see them work!
     {
         "neovim/nvim-lspconfig",
         config = function()
@@ -17,14 +16,22 @@ return {
     {
         "mfussenegger/nvim-jdtls",
     },
-
-    -- {
-    -- 	"nvim-treesitter/nvim-treesitter",
-    -- 	opts = {
-    -- 		ensure_installed = {
-    -- 			"vim", "lua", "vimdoc",
-    --      "html", "css"
-    -- 		},
-    -- 	},
-    -- },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "vim",
+                "lua",
+                "vimdoc",
+                "html",
+                "css",
+                "c",
+                "cpp",
+                "java",
+                "rust",
+                "python",
+                "bash",
+            },
+        },
+    },
 }
