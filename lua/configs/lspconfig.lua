06c2fc6c0723bc70d48afba81df78519dd1c7701
gubnik <danger8217.nik@gmail.com>
2026-01-19 19:04:54 +0300

    Update

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,7 +2,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
 -- some LSPs go by obscure naming, feel free to figure it out on your own
-local servers = { "html", "cssls", "clangd", "asm_lsp", "rust_analyzer", "cmake", "opencl_ls", "gopls", "pylyzer" }
+local servers = { "html", "cssls", "clangd", "asm_lsp", "rust_analyzer", "cmake", "opencl_ls", "gopls", "pyright" }
 
 -- deprecated lspconfig, long live vim.lsp
 vim.lsp.enable(servers)
